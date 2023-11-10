e4c3b2a15128c5f06873bc92086d0daccfdd131e
ivanmarinoff <marinoffivan@gmail.com>
2023-11-10 13:02:23 +0200

    Added new test and some personal credentions

--- test/ui.test.js
+++ test/ui.test.js
@@ -1,8 +1,16 @@
 const { test, expect } = require('@playwright/test');
+const pageUrl = 'http://localhost:3000';
 
 test('Verrifi "All Books" is visible', async ({ page }) => {
-    await page.goto('http://localhost:3000');
+    await page.goto(pageUrl);
     await page.waitForSelector('nav.navbar');
     const allBooksLink = await page.$('a[href="/catalog"]');
     const isAllBookLinkVisible = await allBooksLink.isVisible();
     expect(isAllBookLinkVisible).toBe(true);})
+
+    test('Verrifi Login button is visible', async ({ page }) => {
+        await page.goto(pageUrl);
+        await page.waitForSelector('nav.navbar');
+        const liginButton = await page.$('a[href="/login"]');
+        const isLoginButtonVisible = await liginButton.isVisible();
+        expect(isLoginButtonVisible).toBe(true);})
