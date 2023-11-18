b61008e21ef404e38ccf075d32e7cbaf679d82d8
Ivan Marinov <107050101+ivanmarinoff@users.noreply.github.com>
2023-11-18 15:44:29 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1398,16 +1398,31 @@
     ];
 
     const server = http__default['default'].createServer(requestHandler(plugins, services));
+    let port = process.argv[2];
+    if (!port) port = process.env['PORT'];
+    if (!port) port = 8080;
 
-    const port = 8080;
-    server.listen(port);
-    console.log(`Server started on port ${port}. You can make requests to http://localhost:${port}/`);
-    console.log(`Admin panel located at http://localhost:${port}/admin`);
+    app.listen(port, () => {
+        console.log(`App started. Listening at http://localhost:${port}`);
+})
+    .on('error', function(err) {
+    if (err.errno === 'EADDRINUSE')
+        console.error(`Port ${port} busy.`);
+    else 
+        throw err;
+});
 
-    var softuniPracticeServer = {
+    
 
-    };
+    // const port = 8080;
+    // server.listen(port);
+    // console.log(`Server started on port ${port}. You can make requests to http://localhost:${port}/`);
+    // console.log(`Admin panel located at http://localhost:${port}/admin`);
 
-    return softuniPracticeServer;
+    // var softuniPracticeServer = {
+
+    // };
+
+    // return softuniPracticeServer;
 
 })));
