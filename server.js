9fcbfc107372f2e5f5168ed550b1856bf0a03fc9
ivanmarinoff <marinoffivan@gmail.com>
2023-11-22 11:55:56 +0200

    Updated server port 3000

--- server.js
+++ server.js
@@ -1398,11 +1398,11 @@
     ];
 
     const server = http__default['default'].createServer(requestHandler(plugins, services));
-    const port = 3030;
+    const port = 3000;
     
     server.listen(port);
     console.log(`Server started on port ${port}. You can make requests to http://localhost:${port}/`);
-    console.log(`Admin panel located at http://localhost:${port}/admin`);
+    // console.log(`Admin panel located at http://localhost:${port}/admin`);
 
     var softuniPracticeServer = {
 
